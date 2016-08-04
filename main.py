d5f9469b5f3c8f5507330475eac33da5675e7442
Jared Rada <jared.rada@gmail.com>
2016-08-03 21:16:30 -0400

    example code subscribes to WAMP router

--- main.py
+++ main.py
@@ -1,47 +1,20 @@
-]from autobahn.asyncio.websocket import WebSocketServerProtocol, \
-    WebSocketServerFactory
+from os import environ
+import asyncio
+from autobahn.asyncio.wamp import ApplicationSession, ApplicationRunner
 
 
-class MyServerProtocol(WebSocketServerProtocol):
+class MyComponent(ApplicationSession):
+    @asyncio.coroutine
+    def onJoin(self, details):
+        # listening for the corresponding message from the "backend"
+        # (any session that .publish()es to this topic).
+        def onevent(msg):
+            print("Got event: {}".format(msg))
+        yield from self.subscribe(onevent, u'com.signalk.data')
 
-    def onConnect(self, request):
-        print("Client connecting: {0}".format(request.peer))
 
-    def onOpen(self):
-        print("WebSocket connection open.")
-
-    def onMessage(self, payload, isBinary):
-        if isBinary:
-            print("Binary message received: {0} bytes".format(len(payload)))
-        else:
-            print("Text message received: {0}".format(payload.decode('utf8')))
-
-        # echo back message verbatim
-        self.sendMessage(payload, isBinary)
-
-    def onClose(self, wasClean, code, reason):
-        print("WebSocket connection closed: {0}".format(reason))
 
 
 if __name__ == '__main__':
-
-    try:
-        import asyncio
-    except ImportError:
-        # Trollius >= 0.3 was renamed
-        import trollius as asyncio
-
-    factory = WebSocketServerFactory(u"ws://192.168.1.9:9000")
-    factory.protocol = MyServerProtocol
-
-    loop = asyncio.get_event_loop()
-    coro = loop.create_server(factory, '0.0.0.0', 9000)
-    server = loop.run_until_complete(coro)
-
-    try:
-        loop.run_forever()
-    except KeyboardInterrupt:
-        pass
-    finally:
-        server.close()
-        loop.close()
+    runner = ApplicationRunner(environ.get("AUTOBAHN_DEMO_ROUTER", u"ws://127.0.0.1:8080/ws"),u"realm1",)
+    runner.run(MyComponent)
